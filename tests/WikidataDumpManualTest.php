e12a69a888dbe85de623c96ab6f2d4ed8bb8f686
jeroendedauw <jeroendedauw@gmail.com>
2015-11-11 10:06:22 +0100

    Add manual test file for WD dumps

---
+++ tests/WikidataDumpManualTest.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+use Wikibase\JsonDumpReader\JsonDumpFactory;
+
+$factory = new JsonDumpFactory();
+$iterator = $factory->newStringDumpIterator( $factory->newBz2DumpReader( $argv[1] ) );
+
+foreach ( new \LimitIterator( $iterator, 0, 100 ) as $i => $line ) {
+	echo $i . ': ' . substr( $line, 0, 150 ) . "\n";
+}
