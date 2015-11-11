9ae89ef8d01591fba8e44297571c0d85998e3ec8
jeroendedauw <jeroendedauw@gmail.com>
2015-11-11 11:24:44 +0100

    Increase limit in manual testing script

--- tests/WikidataDumpManualTest.php
+++ tests/WikidataDumpManualTest.php
@@ -7,6 +7,6 @@ use Wikibase\JsonDumpReader\JsonDumpFactory;
 $factory = new JsonDumpFactory();
 $iterator = $factory->newStringDumpIterator( $factory->newBz2DumpReader( $argv[1] ) );
 
-foreach ( new \LimitIterator( $iterator, 0, 100 ) as $i => $line ) {
+foreach ( new \LimitIterator( $iterator, 0, 1010 ) as $i => $line ) {
 	echo $i . ': ' . substr( $line, 0, 150 ) . "\n";
 }
